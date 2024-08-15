da80c776a5b2ed7ce4731fbae793527d022cddfe
jyu-si <syuuji1127@gmail.com>
2024-08-15 19:13:07 +0900

    Update hum.js

--- hum.js
+++ hum.js
@@ -1,1 +1,4 @@
-
+document.querySelector('.openbtn4').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementById('button-container').classList.toggle('show');
+});
