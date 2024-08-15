e6299ed55c419982e1b7c406ad802e1683952a32
jyu-si <syuuji1127@gmail.com>
2024-08-16 02:15:01 +0900

    Update hum.js

--- hum.js
+++ hum.js
@@ -1,4 +1,28 @@
-document.querySelector('.openbtn4').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.getElementById('button-container').classList.toggle('show');
-});
+function setupButtons() {
+    var buttonContainer = document.getElementById('button-container');
+    buttonContainer.innerHTML = ''; // 既存のボタンをクリア
+    imageData.forEach(image => {
+        var button = document.createElement('button');
+        button.className = 'switch-button';
+        button.innerText = image.label;
+        button.onclick = () => switchImage(image.id);
+        buttonContainer.appendChild(button);
+    });
+}
+
+function switchImage(id) {
+    var image = imageData.find(img => img.id === id);
+    if (image) {
+        document.getElementById('image').setAttribute('src', image.src);
+        document.getElementById('label-text').setAttribute('value', image.name);
+        currentImage = id;
+    }
+}
+
+function loadImage(id) {
+    var image = imageData.find(img => img.id === id);
+    if (image) {
+        document.getElementById('image').setAttribute('src', image.src);
+        document.getElementById('label-text').setAttribute('value', image.name);
+    }
+}
