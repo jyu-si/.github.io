30defc1a48dc2f02226abb3660b1ffd570713248
jyu-si <syuuji1127@gmail.com>
2024-08-16 14:26:14 +0900

    Update hum.js

--- hum.js
+++ hum.js
@@ -1,30 +1,49 @@
-// ハンバーガーメニューの開閉機能を定義
-function toggleMenu() {
-  var button = document.querySelector('.openbtn4');
-  var menu = document.getElementById('button-container');
-  button.classList.toggle('active');
-  menu.classList.toggle('show');
-}
+document.addEventListener('DOMContentLoaded', function () {
+    var currentImage = 'matunobe';
+    var imageData = [];
 
-// ページがロードされた後でイベントリスナーを追加
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.openbtn4').addEventListener('click', toggleMenu);
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            imageData = data.images;
+            setupButtons();
+            loadImage('matunobe');
+        })
+        .catch(error => console.error('Error:', error));
+
+    function setupButtons() {
+        var buttonContainer = document.getElementById('button-container');
+        buttonContainer.innerHTML = ''; // 既存のボタンをクリア
+        imageData.forEach(image => {
+            var button = document.createElement('button');
+            button.className = 'switch-button';
+            button.innerText = image.label;  // ボタンのテキストとして "label" を使用
+            button.onclick = () => switchImage(image.id);
+            buttonContainer.appendChild(button);
+        });
+    }
+
+    function switchImage(id) {
+        var image = imageData.find(img => img.id === id);
+        if (image) {
+            document.getElementById('image').setAttribute('src', image.src);
+            document.getElementById('label-text').setAttribute('value', image.name);  // 中央の文字として "name" を使用
+            currentImage = id;
+        }
+    }
+
+    function loadImage(id) {
+        var image = imageData.find(img => img.id === id);
+        if (image) {
+            document.getElementById('image').setAttribute('src', image.src);
+            document.getElementById('label-text').setAttribute('value', image.name);  // 中央の文字として "name" を使用
+        }
+    }
+
+    function goToURL() {
+        var image = imageData.find(img => img.id === currentImage);
+        if (image) {
+            window.location.href = image.url;
+        }
+    }
 });
-
-// 画像の切り替え機能を定義
-function switchImage(id) {
-  var image = imageData.find(img => img.id === id);
-  if (image) {
-    document.getElementById('image').setAttribute('src', image.src);
-    document.getElementById('label-text').setAttribute('value', image.name); // nameを参照
-    currentImage = id;
-  }
-}
-
-function loadImage(id) {
-  var image = imageData.find(img => img.id === id);
-  if (image) {
-    document.getElementById('image').setAttribute('src', image.src);
-    document.getElementById('label-text').setAttribute('value', image.name); // nameを参照
-  }
-}
