e72799b6a096ce921ebc4834085621b87f81ebb2
jyu-si <syuuji1127@gmail.com>
2024-08-16 14:32:43 +0900

    Update hum.js

--- hum.js
+++ hum.js
@@ -1,49 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var currentImage = 'matunobe';
-    var imageData = [];
+function setupButtons() {
+    var buttonContainer = document.getElementById('button-container');
+    buttonContainer.innerHTML = ''; // 既存のボタンをクリア
+    imageData.forEach(image => {
+        var button = document.createElement('button');
+        button.className = 'switch-button';
+        button.innerText = image.label;  // ボタンには "label" を使用
+        button.onclick = () => switchImage(image.id);
+        buttonContainer.appendChild(button);
+    });
+}
 
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            imageData = data.images;
-            setupButtons();
-            loadImage('matunobe');
-        })
-        .catch(error => console.error('Error:', error));
-
-    function setupButtons() {
-        var buttonContainer = document.getElementById('button-container');
-        buttonContainer.innerHTML = ''; // 既存のボタンをクリア
-        imageData.forEach(image => {
-            var button = document.createElement('button');
-            button.className = 'switch-button';
-            button.innerText = image.label;  // ボタンのテキストとして "label" を使用
-            button.onclick = () => switchImage(image.id);
-            buttonContainer.appendChild(button);
-        });
+function switchImage(id) {
+    var image = imageData.find(img => img.id === id);
+    if (image) {
+        document.getElementById('image').setAttribute('src', image.src);
+        document.getElementById('label-text').setAttribute('value', image.id);  // 中央には "id" を表示
+        currentImage = id;
     }
+}
 
-    function switchImage(id) {
-        var image = imageData.find(img => img.id === id);
-        if (image) {
-            document.getElementById('image').setAttribute('src', image.src);
-            document.getElementById('label-text').setAttribute('value', image.name);  // 中央の文字として "name" を使用
-            currentImage = id;
-        }
+function loadImage(id) {
+    var image = imageData.find(img => img.id === id);
+    if (image) {
+        document.getElementById('image').setAttribute('src', image.src);
+        document.getElementById('label-text').setAttribute('value', image.id);  // 初期表示も "id" を表示
     }
-
-    function loadImage(id) {
-        var image = imageData.find(img => img.id === id);
-        if (image) {
-            document.getElementById('image').setAttribute('src', image.src);
-            document.getElementById('label-text').setAttribute('value', image.name);  // 中央の文字として "name" を使用
-        }
-    }
-
-    function goToURL() {
-        var image = imageData.find(img => img.id === currentImage);
-        if (image) {
-            window.location.href = image.url;
-        }
-    }
-});
+}
